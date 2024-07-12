a22c28f247b4303d0d569ac81762eb4d1ae05fdc
arnavgupta00 <arnavguptagg@gmail.com>
2024-07-12 22:22:07 +0530

    final preview 5

--- chat-backend/src/index.ts
+++ chat-backend/src/index.ts
@@ -25,6 +25,7 @@ pgClient
         room VARCHAR(255) NOT NULL,
         username VARCHAR(255) NOT NULL,
         message TEXT NOT NULL,
+        image TEXT,
         timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
     `;
@@ -83,13 +84,14 @@ wss.on("connection", (ws: WebSocket) => {
     }
 
     const query = `
-      INSERT INTO chat (room, username, message, timestamp)
-      VALUES ($1, $2, $3, NOW())
+      INSERT INTO chat (room, username, message, timestamp , image)
+      VALUES ($1, $2, $3, NOW() , $4)
     `;
     const values = [
       chatMessage.room,
       chatMessage.username,
       chatMessage.message,
+      chatMessage.image,
     ];
 
     try {
